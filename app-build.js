9bbff53a505b3ce71711f4a4e5830e05f0021532
kobycarson67 <koby.carson67@gmail.com>
2026-09-18 02:12:50 -0600

    Bump ARC checkpoint roster build marker

--- app-build.js
+++ app-build.js
@@ -1,7 +1,7 @@
 (function(root){
   root.ARC_BUILD=Object.freeze({
     version:'0.18',
-    build:'pwa-update-controls-1',
-    shell:'v0-18-samsung-stabilization-1-booth-v1-project-bank-v1-update-controls-1'
+    build:'checkpoint-roster-state-1',
+    shell:'v0-18-samsung-stabilization-1-booth-v1-project-bank-v1-update-controls-1-checkpoint-roster-1'
   });
 })(typeof self!=='undefined'?self:this);
